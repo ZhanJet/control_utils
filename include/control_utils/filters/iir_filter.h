7d5a8ecca93d550e4c26f9b6c1a8a3f5afd9b6d7
zhanjet <zhanjet@airs.com>
2021-03-14 23:50:15 +0800

    create repository and add iir_filter

---
+++ include/control_utils/filters/iir_filter.h
@@ -0,0 +1,137 @@
+/**
+ Copyright (c) 2021, Zhangjie Tu.
+ All rights reserved.
+  @author zhanjet
+ */
+
+#ifndef IIR_FILTER_HPP
+#define IIR_FILTER_HPP
+
+#include <math.h>
+#include <vector>
+
+class IirFilterBw
+{
+private:
+    int order_;
+    // cutoff_freq: default -1 corresponds to a cutoff frequency at 1/4 sampling rate
+    double cutoff_freq_, zeta_;
+    double c_, tan_flt_, sampling_rate_;
+    std::vector<double> xk_, xk_filtered_, xk_diff_, xk_diff_filtered_;
+
+public:
+    // IirFilterBw2rd():cutoff_freq_(-1), xk_(3,0), xk_filtered_(3,0), xk_diff_(3,0), xk_diff_filtered_(3,0){}
+    /**
+     * @brief Construct a new IirFilterBw object
+     * 
+     * @param order: order of iirfilter, value in 1 or 2
+     * @param cutoff_freq: in Hz
+     */
+    IirFilterBw(const int order, const double sampling_rate, const double cutoff_freq): 
+                order_(order), sampling_rate_(sampling_rate), cutoff_freq_(cutoff_freq), zeta_(1.),
+                xk_(order+1,0.), xk_filtered_(order+1,0.), xk_diff_(order+1,0.), xk_diff_filtered_(order+1,0.){}
+    ~IirFilterBw(){}
+
+    void setSamplingRate(const double sampling_rate){
+        sampling_rate_ = sampling_rate;
+    }
+
+    void setCutoffFreq(const double cutoff_freq){
+        cutoff_freq_ = cutoff_freq;
+        if(cutoff_freq_ < 0.0) cutoff_freq_ = -1;
+    }
+
+    void setZeta(const double zeta){
+        zeta_ = zeta;
+        if(zeta_ < 0.0) zeta_ = 1;
+    }
+
+    double update(const double delta_t, const double xk){
+        if(cutoff_freq_ > 0.0){
+            // tan_flt_ = tan((cutoff_freq_ * 2.0 * M_PI) * delta_t / 2.0);
+            // tan_flt_ = tan(cutoff_freq_ * M_PI * delta_t);
+            tan_flt_ = tan(cutoff_freq_ * M_PI / sampling_rate_);
+            // check if tan(0)
+            if(tan_flt_ < 0.01)
+                tan_flt_ = 0.01;
+            
+            c_ = 1.0 / tan_flt_;
+        } else {
+            c_ = 1.0;
+        }
+
+        if(1 == order_) {
+            xk_.at(1) = xk_.at(0);
+            xk_.at(0) = xk;
+
+            xk_filtered_.at(1) = xk_filtered_.at(0);
+            xk_filtered_.at(0) = (1 / (1 + c_)) * (xk_.at(0) + xk_.at(1) - (1 - c_) * xk_filtered_.at(1));
+        }
+
+        if(2 == order_) {
+            xk_.at(2) = xk_.at(1);
+            xk_.at(1) = xk_.at(0);
+            xk_.at(0) = xk;
+
+            xk_filtered_.at(2) = xk_filtered_.at(1);
+            xk_filtered_.at(1) = xk_filtered_.at(0);
+            // xk_filtered_.at(0) = 
+            //     (1 / (1 + c_ * c_ + 1.414 * c_)) * (xk_.at(2) + 2 * xk_.at(1) + xk_.at(0) -
+            //                             (c_ * c_ - 1.414 * c_ + 1) * xk_filtered_.at(2) -
+            //                             (-2 * c_ * c_ + 2) * xk_filtered_.at(1));
+            xk_filtered_.at(0) = 
+                (1 / (1 + c_ * c_ + 2*zeta_ * c_)) * (xk_.at(2) + 2 * xk_.at(1) + xk_.at(0) -
+                                        (c_ * c_ - 2*zeta_ * c_ + 1) * xk_filtered_.at(2) -
+                                        (-2 * c_ * c_ + 2) * xk_filtered_.at(1));
+        }
+
+        return xk_filtered_.at(0);
+    }
+
+    double update_dx(const bool has_update, const double delta_t, const double xk){
+
+        if(!has_update){
+            update(delta_t, xk);
+        }
+
+        if(1 == order_) {
+            xk_diff_filtered_.at(0) = (xk_filtered_.at(0) - xk_filtered_.at(1)) / delta_t;
+        }
+
+        if(2 == order_) {
+            xk_diff_filtered_.at(0) = (xk_filtered_.at(0) - xk_filtered_.at(1)) / delta_t;
+            
+            // xk_diff_.at(2) = xk_diff_.at(1);
+            // xk_diff_.at(1) = xk_diff_.at(0);
+            // xk_diff_.at(0) = (xk_.at(0) - xk_.at(1)) / delta_t;
+
+            // xk_diff_filtered_.at(2) = xk_diff_filtered_.at(1);
+            // xk_diff_filtered_.at(1) = xk_diff_filtered_.at(0);
+            // // xk_diff_filtered_.at(0) = (1 / (1 + c_ * c_ + 1.414 * c_)) *
+            // //                             (xk_diff_.at(2) + 2 * xk_diff_.at(1) + xk_diff_.at(0) -
+            // //                             (c_ * c_ - 1.414 * c_ + 1) * xk_diff_filtered_.at(2) - (-2 * c_ * c_ + 2) * xk_diff_filtered_.at(1));
+            
+            // xk_diff_filtered_.at(0) = (1 / (1 + c_ * c_ + 2*zeta_ * c_)) *
+            //                             (xk_diff_.at(2) + 2 * xk_diff_.at(1) + xk_diff_.at(0)
+            //                             - (c_ * c_ - 2*zeta_ * c_ + 1) * xk_diff_filtered_.at(2)
+            //                             - (-2 * c_ * c_ + 2) * xk_diff_filtered_.at(1));
+
+        }
+        
+        return xk_diff_filtered_.at(0);
+    }
+};
+
+
+// class IirFilterDynCutoff
+// {
+// private:
+
+// public:
+//     IirFilterDynCutoff(const int filter_order, const double sample_freq);
+//     ~IirFilterDynCutoff();
+
+
+// }
+
+#endif
---
+++ test/filter_test.cpp
@@ -0,0 +1,74 @@
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <cmath>
+#include <std_msgs/Float64.h>
+#include "moying_motion_interface/iir_filter.hpp"
+
+double input;
+
+void input_cb(const std_msgs::Float64::ConstPtr& msg) {
+    input = msg->data;
+}
+
+double sign(double arg) {
+    return (arg<0)?(-1):(1);
+}
+
+int main(int argc, char** argv) {
+
+    ros::init(argc, argv, "filter_test");
+    ros::NodeHandle nh;
+
+    ros::Subscriber input_sub = nh.subscribe("filter_input", 1, input_cb);
+    ros::Publisher filter_x_pub = nh.advertise<std_msgs::Float64>("filter_x", 1);
+    ros::Publisher filter_dx_pub = nh.advertise<std_msgs::Float64>("filter_dx", 1);
+    ros::Publisher deltat_pub = nh.advertise<std_msgs::Float64>("delta_t", 1);
+    ros::Publisher setpoint_pub = nh.advertise<std_msgs::Float64>("setpoint", 1);
+
+    ros::Rate rate(50.0);
+    double cutoff_freq = 10.0;
+    // double zeta = 1.0;
+    // IirFilterBw2rd* filter = new IirFilterBw2rd(cutoff_freq, zeta);
+    IirFilterBw* filter = new IirFilterBw(2, 50, cutoff_freq);
+    ros::Time prev_time, start_time;
+    ros::Duration delta_t, sim_time;
+    std_msgs::Float64 x, dx, delta_t_topic;
+    std_msgs::Float64 setpoint;
+    // setpoint.data = 1.0;
+    start_time = ros::Time::now();
+    while (ros::ok())
+    {
+        ros::spinOnce();
+
+        // calculate delta_t
+        if(!prev_time.isZero()){
+            delta_t = ros::Time::now() - prev_time;
+            prev_time = ros::Time::now();
+            if(0 == delta_t.toSec()){
+                ROS_ERROR("delta_t is 0, skipping this loop.");
+                continue;
+            }
+        } else {
+            ROS_INFO("prev_time is 0, doing nothing");
+            prev_time = ros::Time::now();
+            continue;
+        }
+
+        // x.data = filter->update(delta_t.toSec(), input);
+        // dx.data = filter->update_dx(true, delta_t.toSec(), input);
+        sim_time = ros::Time::now() - start_time;
+        setpoint.data = sign(std::sin(sim_time.toSec()))*1.0 + 0*std::sin(sim_time.toSec());
+        // if(sim_time.toSec() / 2 < 1e-2) setpoint.data = 0.0 - setpoint.data;
+        x.data = filter->update(delta_t.toSec(), setpoint.data);
+        dx.data = filter->update_dx(true, delta_t.toSec(), setpoint.data);
+
+        filter_x_pub.publish(x);
+        filter_dx_pub.publish(dx);
+        delta_t_topic.data = delta_t.toSec();
+        deltat_pub.publish(delta_t_topic);
+        setpoint_pub.publish(setpoint);
+
+        rate.sleep();
+
+    }
+}
